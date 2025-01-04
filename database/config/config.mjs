901edca2b36d0538ec648265b6f21991bdb45676
Kali-Venum <subhamdas.abcd@gmail.com>
2025-01-04 10:45:58 +0530

    sequelize setup done.

---
+++ database/config/config.mjs
@@ -0,0 +1,48 @@
+// {
+//   "development": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_development",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "test": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_test",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   },
+//   "production": {
+//     "username": "root",
+//     "password": null,
+//     "database": "database_production",
+//     "host": "127.0.0.1",
+//     "dialect": "mysql"
+//   }
+// }
+
+export const options = {
+  username: process.env.DATABASE_USERNAME,
+  password: process.env.DATABASE_PASSWORD,
+  database: process.env.DATABASE_NAME,
+  host: process.env.DATABASE_HOST,
+  port: Number(process.env.DATABASE_PORT),
+  dialect: process.env.DATABASE_DIALECT,
+  logging: process.env.NODE_ENV === "development" ? true : false,
+  migrationStorageTableName: "migrations",
+};
+
+if (process.env.NODE_ENV === "production") {
+  options.dialectOptions = {
+    ssl: {
+      rejectUnauthorized: true,
+    },
+  };
+}
+
+export default {
+  development: options,
+  test: options,
+  production: options,
+};
---
+++ src/app/api/signup/route.ts
@@ -0,0 +1,42 @@
+import { NextRequest, NextResponse } from "next/server";
+import bcrypt from "bcryptjs";
+
+export async function POST(req: NextRequest) {
+  try {
+    // Read the data from the request body
+    const body = await req.json();
+    
+    // Ensure the body is an object and not null
+    if (!body || typeof body !== "object") {
+      throw new TypeError(
+        'The "payload" argument must be of type object. Received null'
+      );
+    }
+
+    // Extracting data from the body
+    const { name, email, password } = body;
+
+    // Hashing the password
+    const hashedPassword = await bcrypt.hash(password, 8);
+
+    // Create a user in db
+    
+    console.log(newUser, "newUser");
+
+    return NextResponse.json({
+      message: "User created successfully",
+      statusCode: 201,
+      data: {
+        user: newUser,
+      },
+    });
+  } catch (error: any) {
+    console.log(error, "error");
+
+    return NextResponse.json({
+      message: "An error occurred",
+      statusCode: 500,
+      error: error.message,
+    });
+  }
+}
